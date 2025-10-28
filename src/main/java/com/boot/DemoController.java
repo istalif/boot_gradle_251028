b31331807d37727b37250a40126421c4c82e86f1
istalif <bread998@naver.com>
2025-10-28 15:04:31 +0900

    111

--- src/main/java/com/boot/DemoController.java
+++ src/main/java/com/boot/DemoController.java
@@ -19,7 +19,7 @@ public class DemoController {
 
     @RequestMapping("/hello.do")
     public String hello(Model model){
-        log.info("안녕하세요!");
+        log.info("안녕하세요! 111");
         model.addAttribute("message","hello.jsp 입니다!");
 
         return "hello";
